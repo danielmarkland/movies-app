65959caf3d7edf43a1544c4941b13f94a3df49cd
Daniel Markland <daniel@danielmarkland.com>
2025-12-13 23:49:40 -0600

    Removed React Query dependency

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,31 +1,16 @@
 "use client"
 
 import { ThemeProvider } from "next-themes"
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from "react"
 import { TooltipProvider } from "@radix-ui/react-tooltip"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60_000,
-        refetchOnWindowFocus: false
-      }
-    }
-  }))
-    
   return (
     <ThemeProvider
       attribute="class"
       defaultTheme="system"
       enableSystem
     >
-      <TooltipProvider>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </TooltipProvider>
+      <TooltipProvider>{children}</TooltipProvider>
     </ThemeProvider>
   )
 }
